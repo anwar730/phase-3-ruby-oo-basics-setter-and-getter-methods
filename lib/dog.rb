ecdd878a8e71500c0845643fee341d70e545dd60
anwarismail <anwaarismail449@gmail.com>
2022-10-17 13:35:15 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
 
+    attr_accessor :name
+    attr_accessor :breed
+    def get_name=(name)
+        @name = name
+        
+    end
+
+    def get_breed=(breed)
+        @breed = breed
+    end    
+    
+
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
 
+    attr_accessor :name
+    attr_accessor :job
+
+    def get_name=(name)
+        @name=name
+    end    
+
+    def get_job=(job)
+        @job= job
+    end    
+
 end
